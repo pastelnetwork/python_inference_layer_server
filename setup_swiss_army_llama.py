5a1c5ae79f33b1a0359e56fa8a253ea5031abf22
Dicklesworthstone <jeff141421@gmail.com>
2024-04-30 19:40:33 -0400

    x

--- setup_swiss_army_llama.py
+++ setup_swiss_army_llama.py
@@ -22,12 +22,30 @@ def get_external_ip_func():
     logger.warning("Failed to retrieve external IP address from all providers.")
     return "Unknown"
 
-def is_port_available(port):
+def run_command(command, env=None, capture_output=False, check=False):
+    shell = '/bin/zsh' if os.path.exists('/bin/zsh') else '/bin/bash'
+    if env:
+        full_env = {**os.environ, **env}
+    else:
+        full_env = os.environ.copy()
+    command = ' '.join(command) if isinstance(command, list) else command
     try:
-        result = subprocess.run(["lsof", "-i", f":{port}"], capture_output=True)
-        return result.returncode != 0
-    except:  # noqa: E722
-        return True
+        result = subprocess.run(command, shell=True, env=full_env, capture_output=capture_output, text=True, executable=shell, check=check)
+        if capture_output:
+            if result.stdout:
+                logger.info(result.stdout)
+            if result.stderr:
+                logger.error(result.stderr)
+        return result
+    except subprocess.CalledProcessError as e:
+        logger.error(f"Command '{command}' failed with exit code {e.returncode}")
+        if capture_output:
+            logger.error(e.output)
+        raise
+    
+def is_port_available(port):
+    result = run_command(["lsof", "-i", f":{port}"], capture_output=True)
+    return result.returncode != 0
 
 def is_swiss_army_llama_responding(swiss_army_llama_port: int, security_token: str):
     try:
@@ -39,173 +57,140 @@ def is_swiss_army_llama_responding(swiss_army_llama_port: int, security_token: s
         print(f"Error: {e}")
         return False
     
-def run_command(command, shell='/bin/bash', env=None):
-    """Runs a command through subprocess.run with specified shell and environment."""
-    subprocess.run(command, shell=True, executable=shell, env=env)
-
-def is_pyenv_installed():
-    """Checks if pyenv is installed by attempting to run 'pyenv --version' in a subshell."""
-    shell_path = os.environ.get('SHELL', '/bin/bash')
-    profile_file = "zshrc" if "/zsh" in shell_path else "bashrc"
-    command = f"source ~/.{profile_file} && pyenv --version"
-    try:
-        subprocess.run(command, shell=True, executable=shell_path, check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-        return True
-    except (FileNotFoundError, subprocess.CalledProcessError):
-        return False
+def update_security_token(file_path, token):
+    with open(file_path, 'r+') as file:
+        content = file.read()
+        file.seek(0)
+        file.truncate()
+        content = re.sub(r'SECURITY_TOKEN\s*=\s*"[^"]*"', f'SECURITY_TOKEN = "{token}"', content)
+        file.write(content)
     
+def is_pyenv_installed():
+    pyenv_init_script = os.path.expanduser("~/.pyenv/bin/pyenv")
+    if os.path.isfile(pyenv_init_script):
+        result = run_command([pyenv_init_script, '--version'], capture_output=True)
+        return result.returncode == 0
+    return False
+
 def is_python_3_12_installed():
-    try:
-        result = subprocess.run(["pyenv", "versions", "--bare"], capture_output=True, text=True)
-        return "3.12" in result.stdout
-    except:  # noqa: E722
-        return False
+    pyenv_root = os.path.expanduser("~/.pyenv")
+    pyenv_versions = os.path.join(pyenv_root, "versions")
+    return "3.12" in os.listdir(pyenv_versions)
 
 def is_rust_installed():
-    """Check if Rust is installed by attempting to run `rustc --version`."""
     try:
-        subprocess.run(["rustc", "--version"], capture_output=True, check=True)
-        return True
-    except subprocess.CalledProcessError:
+        result = run_command(["rustc", "--version"], capture_output=True)
+        return result.returncode == 0
+    except FileNotFoundError:
         return False
-    
+
+def setup_virtual_environment(swiss_army_llama_path):
+    venv_path = os.path.join(swiss_army_llama_path, 'venv')
+    python_executable = os.path.join(venv_path, 'bin', 'python')
+    pip_executable = os.path.join(venv_path, 'bin', 'pip')
+    run_command([f'python3 -m venv {venv_path}'], check=True)
+    run_command([f'{pip_executable} install --upgrade pip'], check=True)
+    run_command([f'{pip_executable} install -r requirements.txt'], check=True)
+    return python_executable
+
 def set_timezone_utc():
-    """
-    Sets the timezone to UTC for the current Python session and permanently for the system.
-    For permanent setting, it appends `export TZ=UTC` to the user's shell profile file.
-    """
-    # Set timezone to UTC for the current session
     os.environ['TZ'] = 'UTC'
-    # Determine the user's shell profile file
-    shell_profile_path = os.path.expanduser('~/.bashrc')  # Default to .bashrc
-    if os.path.exists(os.path.expanduser('~/.zshrc')):
-        shell_profile_path = os.path.expanduser('~/.zshrc')  # Use .zshrc if it exists
-    # Command to append TZ=UTC export to the shell profile
-    append_tz_command = f'echo "export TZ=UTC" >> {shell_profile_path}'
-    # Check if TZ=UTC is already in the profile to avoid duplicates
-    try:
-        with open(shell_profile_path, 'r') as shell_profile:
-            if 'export TZ=UTC' not in shell_profile.read():
-                # Append the export command to the profile
-                subprocess.run(append_tz_command, shell=True)
-    except Exception as e:
-        logger.error(f"An error occurred while setting timezone: {e}")    
-    
+    shell_profile_path = os.path.expanduser('~/.zshrc') if os.path.exists(os.path.expanduser('~/.zshrc')) else os.path.expanduser('~/.bashrc')
+    if 'export TZ=UTC' not in open(shell_profile_path, 'r').read():
+        run_command([f'echo "export TZ=UTC" >> {shell_profile_path}']) 
+
+def check_systemd_service_exists(service_name):
+    result = run_command(f"systemctl is-enabled {service_name}", capture_output=True)
+    return result.returncode == 0 and 'enabled' in result.stdout
+
+def create_systemd_service(service_name, user, working_directory, exec_start):
+    service_content = f"""[Unit]
+Description=Swiss Army Llama service
+After=network.target
+
+[Service]
+Type=simple
+User={user}
+WorkingDirectory={working_directory}
+ExecStart={exec_start}
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+"""
+    service_path = f"/etc/systemd/system/{service_name}.service"
+    if os.geteuid() == 0:
+        with open(service_path, 'w') as file:
+            file.write(service_content)
+        logger.info(f"Systemd service file created at {service_path}")
+        run_command("systemctl daemon-reload")
+        run_command(f"systemctl enable {service_name}")
+        run_command(f"systemctl start {service_name}")
+    else:
+        logger.error("You need root permissions to create systemd services.")
+
+def ensure_pyenv_setup():
+    if not is_pyenv_installed():
+        logger.info("Installing pyenv...")
+        run_command(["sudo apt-get update && sudo apt-get install -y build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python3-openssl git redis redis-tools"], check=True)
+        run_command(["curl https://pyenv.run | bash"], check=True)
+    if not is_python_3_12_installed():
+        logger.info("Installing Python 3.12 using pyenv...")
+        run_command(["pyenv install 3.12"], check=True)
+        run_command(["pyenv global 3.12"], check=True)
+
+def setup_python_environment(swiss_army_llama_path):
+    pip_commands = f"source {swiss_army_llama_path}/venv/bin/activate && pip install --upgrade pip && pip install wheel && pip install -r {swiss_army_llama_path}/requirements.txt"
+    run_command([pip_commands])
+        
+def configure_shell_for_pyenv():
+    shell_rc_path = os.path.expanduser("~/.zshrc") if os.path.exists(os.path.expanduser("~/.zshrc")) else os.path.expanduser("~/.bashrc")
+    pyenv_init_str = 'export PYENV_ROOT="$HOME/.pyenv"\nexport PATH="$PYENV_ROOT/bin:$PATH"\neval "$(pyenv init --path)"\n'
+    if pyenv_init_str not in open(shell_rc_path).read():
+        with open(shell_rc_path, "a") as shell_rc:
+            shell_rc.write(pyenv_init_str)
+    os.environ["PYENV_ROOT"] = os.path.expanduser("~/.pyenv")
+    os.environ["PATH"] = f"{os.environ['PYENV_ROOT']}/bin:{os.environ.get('PATH', '')}"
+        
 def setup_swiss_army_llama(security_token):
     set_timezone_utc()
     swiss_army_llama_path = os.path.expanduser("~/swiss_army_llama")
+    swiss_army_llama_script = os.path.join(swiss_army_llama_path, "swiss_army_llama.py")
     if not os.path.exists(swiss_army_llama_path):
-        logger.info("Changing to the home directory.")
-        os.chdir(os.path.expanduser("~"))
         logger.info("Cloning the Swiss Army Llama repository.")
-        command = "git clone https://github.com/Dicklesworthstone/swiss_army_llama"
-        logger.info(f"Now running command: {command}")
-        subprocess.run(command, shell=True, executable="/bin/bash")
+        run_command(f"git clone https://github.com/Dicklesworthstone/swiss_army_llama {swiss_army_llama_path}", check=True)
     else:
-        logger.info("Swiss Army Llama repository already exists.")
-        logger.info("Pulling the latest changes from the repository.")
-        os.chdir(swiss_army_llama_path)
-        command = "git stash && git pull"
-        logger.info(f"Now running command: {command}")
-        subprocess.run(command, shell=True, executable="/bin/bash")
-    logger.info("Updating the Swiss Army Llama code file with the provided security token.")
-    swiss_army_llama_file_path = os.path.join(swiss_army_llama_path, "swiss_army_llama.py")
-    with open(swiss_army_llama_file_path, "r") as file:
-        code_content = file.read()
-    code_content = re.sub(r'SECURITY_TOKEN\s*=\s*"[^"]+"', f'SECURITY_TOKEN = "{security_token}"', code_content)
-    code_content = re.sub(r'use_hardcoded_security_token\s*=\s*\d+', 'use_hardcoded_security_token = 1', code_content)
-    with open(swiss_army_llama_file_path, "w") as file:
-        file.write(code_content)
-    logger.info("Checking for pyenv installation.")
-    if not is_pyenv_installed():
-        logger.info("pyenv is not installed. Installing pyenv.")
-        apt_get_install = "sudo apt-get update && sudo apt-get install -y build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python3-openssl git redis redis-tools"
-        run_command(apt_get_install)
-        logger.info("Installing pyenv.")
-        pyenv_clone = "git clone https://github.com/pyenv/pyenv.git ~/.pyenv"
-        pyenv_dir = os.path.expanduser("~/.pyenv")
-        if not os.path.exists(pyenv_dir):
-            run_command(pyenv_clone)
-        else:
-            logger.info("~/.pyenv already exists. Skipping clone.")
-        shell_rc_path = os.path.expanduser("~/.zshrc") if os.path.exists(os.path.expanduser("~/.zshrc")) else os.path.expanduser("~/.bashrc")
-        with open(shell_rc_path, "a") as shell_rc:
-            shell_rc.write('\nexport PYENV_ROOT="$HOME/.pyenv"\n')
-            shell_rc.write('export PATH="$PYENV_ROOT/bin:$PATH"\n')
-            shell_rc.write('eval "$(pyenv init --path)"\n')
-        # Manually update os.environ for subsequent commands in this script
-        os.environ["PYENV_ROOT"] = os.path.expanduser("~/.pyenv")
-        os.environ["PATH"] = f"{os.path.expanduser('~/.pyenv/bin')}:{os.environ.get('PATH', '')}"
-        logger.info(f"Added pyenv initialization to {shell_rc_path}. Attempting to source it.")
-    else:
-        logger.info("pyenv is already installed.")
+        logger.info("Swiss Army Llama repository already exists. Updating repository.")
+        run_command(f"git -C {swiss_army_llama_path} stash && git -C {swiss_army_llama_path} pull", check=True)
+    ensure_pyenv_setup()
     if not is_python_3_12_installed():
-        logger.info("Python 3.12 is not installed. Installing the latest version of Python 3.12.")
-        commands = [
-            "pyenv install 3.12",
-            "pyenv local 3.12"
-        ]
-        for command in commands:
-            logger.info(f"Now running command: {command}")
-            subprocess.run(command, shell=True, executable="/bin/bash")
-    else:
-        logger.info("Python 3.12 is already installed.")
-    logger.info("Creating and activating virtual environment in ~/swiss_army_llama.")
-    os.chdir(swiss_army_llama_path)
-    # Initialize the commands list with Python environment setup commands
-    commands = [
-        "python -m venv venv",
-        "source venv/bin/activate",
-        "pip install --upgrade pip",
-        "pip install wheel",
-        "pip install -r requirements.txt"
-    ]
+        logger.info("Python 3.12 is not installed. Installing Python 3.12 using pyenv.")
+        run_command("pyenv install 3.12", check=True)
+        run_command("pyenv global 3.12", check=True)
+    update_security_token(swiss_army_llama_script, security_token)
+    python_executable = setup_virtual_environment(swiss_army_llama_path)
     if not is_rust_installed():
         logger.info("Rust is not installed. Installing Rust.")
-        # Prepend Rust installation and setup commands to the commands list
-        rust_commands = [
-            "curl https://sh.rustup.rs -sSf | sh -s -- -y",  # Install Rust non-interactively
-            "source $HOME/.cargo/env",  # Ensure Rust environment is set up for subsequent commands
-            "rustup default nightly",
-            "rustup update nightly"
-        ]
-        commands = rust_commands + commands  # Combine Rust commands with the rest
-    for command in commands:
-        logger.info(f"Now running command: {command}")
-        if command.startswith("source"):
-            command_to_run = command.replace("source", ".", 1)  # Replace 'source' with '.' for bash compatibility
-            subprocess.run(command_to_run, shell=True, executable="/bin/bash", env={"PATH": f"{swiss_army_llama_path}/venv/bin:{os.environ['PATH']}"})
-        else:
-            subprocess.run(command, shell=True, executable="/bin/bash", env={"PATH": f"{swiss_army_llama_path}/venv/bin:{os.environ['PATH']}"})
-    logger.info("Running Swiss Army Llama...")
-    command = "python swiss_army_llama.py"
-    logger.info(f"Now running command: {command}")
-    process = subprocess.Popen(
-        command,
-        shell=True,
-        executable="/bin/bash",
-        env={"PATH": f"{swiss_army_llama_path}/venv/bin:{os.environ['PATH']}"},
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        text=True
-    )
-    external_ip = get_external_ip_func()
-    logger.info(f"Setup complete. Open a browser to {external_ip}:8089 to get to the FastAPI Swagger page. Note that your security token is {security_token}.")
-    # Read and log the output of the Swiss Army Llama process
-    for line in process.stdout:
-        logger.info(f"Swiss Army Llama: {line.strip()}")
-    # Read and log any error output of the Swiss Army Llama process
-    for line in process.stderr:
-        logger.error(f"Swiss Army Llama Error: {line.strip()}")
-    # Wait for the Swiss Army Llama process to finish (if needed)
-    process.wait()        
+        run_command("curl https://sh.rustup.rs -sSf | sh -s -- -y", check=True)
+        os.environ.update({
+            'PATH': f"{os.environ.get('HOME')}/.cargo/bin:{os.environ.get('PATH')}"
+        })
+        run_command("rustup default nightly && rustup update nightly", check=True)
+    setup_python_environment(swiss_army_llama_path)
+    if not check_systemd_service_exists("swiss_army_llama"):
+        create_systemd_service("swiss_army_llama", os.getlogin(), swiss_army_llama_path, f"{python_executable} {swiss_army_llama_script}")
+    else:
+        logger.info("Swiss Army Llama systemd service already exists.")
 
 def check_and_setup_swiss_army_llama(security_token):
     swiss_army_llama_port = 8089
-    if is_swiss_army_llama_responding(swiss_army_llama_port, security_token):
+    external_ip = get_external_ip_func()
+    if external_ip == "Unknown":
+        logger.error("Unable to reach external network providers. Network may be unreachable.")
+        return
+    if is_swiss_army_llama_responding(external_ip, swiss_army_llama_port, security_token) and not is_port_available(swiss_army_llama_port):
         logger.info("Swiss Army Llama is already set up and running.")
+    elif not is_port_available(swiss_army_llama_port):
+        logger.error(f"Port {swiss_army_llama_port} is not available. It may be in use by another process.")
     else:
-        if not is_port_available(swiss_army_llama_port):
-            logger.error(f"Port {swiss_army_llama_port} is not available. Please ensure that Swiss Army Llama is not running on another process.")
-            return
         setup_swiss_army_llama(security_token)
